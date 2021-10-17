cb8873a6d17d70aa945c7e9f189f10be72eccb08
xiuyuan <xiuyuan206@gmail.com>
2021-10-17 14:24:56 -0700

    finished music department scraper

---
+++ scrapers/music_department_scraper.py
@@ -0,0 +1,151 @@
+from requests.models import codes
+from models import Professor, Room
+from autoscraper import AutoScraper
+import os
+from scraper import registry, Scraper
+from bs4 import BeautifulSoup as bsp
+import requests
+
+# 1. Register your scraper so that it gets run by the scrape_data file. Make sure your class is a subclass of `Scraper`
+@registry.register
+class MusicDepartmentScraper(Scraper):
+    """
+    Fetches information about Cal Poly Music Department professors
+    """
+
+    # TODO: fix repetitive code
+    # TODO: ReferenceField
+    # TODO: Kramer sax shows up twice
+
+    links = {"Music": "https://music.calpoly.edu/faculty/"}
+
+    # 2. The only requirement of the class is the `scrape()` function. This will get data from the internet and upload it to the server.
+    def scrape(self):
+        name_bio_dict = (
+            {}
+        )  # used to deal with professors appearing multiple times, with different bios
+
+        page = requests.get("https://music.calpoly.edu/faculty/")
+        soup = bsp(page.content, "html.parser")
+
+        """Faculty Table + Staff Table"""
+        professor_data_html = soup.select("#mainLeftFull > table")
+        four_column_tables = [0, 3]  # faculty and staff
+        for table in four_column_tables:
+            sub_soup = bsp(str(professor_data_html[table]), "html.parser")
+            professor_data = sub_soup.find_all(
+                "tr"
+            )  # only looks at this specific table
+            for prof in professor_data:
+                attributes = prof.find_all(
+                    "td"
+                )  # list of attributes for a prof, has HTML Tags
+                if len(attributes) > 0:
+                    name = attributes[0].text.strip()
+                    office = self.office_str_to_room(attributes[1].text.strip())
+                    email = attributes[2].text.strip()
+                    phone_number = attributes[3].text.strip()
+                    bio = attributes[4].text.strip()  # "music area"
+                    try:
+                        prof = Professor.objects.get(name=name)
+                        prof.office = self.office_str_to_room(office)
+                        prof.email = email
+                        prof.phone_number = phone_number
+                        prof.save()
+                    except:
+                        Professor(
+                            name=name,
+                            office=office,
+                            email=email,
+                            phone_number=phone_number,
+                        ).save()
+                    self.add_to_bio_dict(name_bio_dict, name, bio)
+
+        """Faculty Lecturers"""
+        professor_data_html = soup.select("#mainLeftFull > table:nth-child(5) > tbody")
+        sub_soup = bsp(str(professor_data_html[0]), "html.parser")
+        professor_data = sub_soup.find_all("tr")  # only looks at this specific table
+
+        for prof in professor_data:
+            attributes = prof.find_all(
+                "td"
+            )  # list of attributes for a prof, has HTML Tags
+            if len(attributes) > 0:
+                name = attributes[0].text.strip()
+                office = self.office_str_to_room(attributes[1].text.strip())
+                email = attributes[2].text.strip()
+                bio = attributes[3].text.strip()  # "music area"
+                try:
+                    prof = Professor.objects.get(name=name)
+                    prof.office = self.office_str_to_room(office)
+                    prof.email = email
+                    prof.save()
+                except:
+                    Professor(name=name, office=office, email=email).save()
+                self.add_to_bio_dict(name_bio_dict, name, bio)
+
+        """Applied Faculty Table"""
+        professor_data_html = soup.select(
+            "#mainLeftFull > table:nth-child(8)"
+        )  # .find_all("tr")
+        sub_soup = bsp(str(professor_data_html[0]), "html.parser")
+        professor_data = sub_soup.find_all("tr")  # only looks at this specific table
+
+        for prof in professor_data:
+            attributes = prof.find_all(
+                "td"
+            )  # list of attributes for a prof, has HTML Tags
+
+            if len(attributes) > 0:
+                name = attributes[0].text.strip()
+                bio = attributes[1].text.strip()
+                try:
+                    prof = Professor.objects.get(name=name)
+                    prof.save()
+                except:
+                    Professor(name=name).save()
+                self.add_to_bio_dict(name_bio_dict, name, bio)
+                x = set()
+
+        for name, bio in name_bio_dict.items():
+            bio_text = "Music Area: " + ", ".join(bio)
+            try:
+                prof = Professor.objects.get(name=name)
+                prof.bio = bio_text
+                prof.save()
+            except:
+                Professor(name=name, bio=bio_text).save()
+
+    def add_to_bio_dict(self, name_bio_dict, name, bio):
+        """creates a key-value pair between name and bio in name_bio_dict
+        if key exists, appends bio to existing bio"""
+        if name in name_bio_dict:
+            bio_set = name_bio_dict[name]
+            bio_set.update(bio.split(", "))
+        else:
+            bio_set = set()
+            bio_set.update(bio.split(", "))
+            name_bio_dict[name] = bio_set
+
+    def office_str_to_room(self, office):
+        """converts office strings (e.g. '45-121') into room objects"""
+        # TODO: building number to building name utils?
+        # CURRENTLY BUILDING NAME IS JUST ITS NUMBER
+        split_string = office.split("-")
+        if len(split_string) == 2:
+            building_number = split_string[0]
+            room_number = split_string[1]
+        elif len(split_string) == 1 and split_string[0] != "":
+            building_number = office  # to handle the office = "45M" case
+            room_number = office
+        else:
+            return None
+        try:
+            room = Room.objects.get(
+                building_name=building_number, room_number=room_number
+            )
+            return room
+        except:
+            room = Room(building_name=building_number, room_number=room_number)
+            room.save()
+            return room
--- tests/local_database.py
+++ tests/local_database.py
@@ -3,7 +3,10 @@ from mongoengine import connect
 
 from models import *
 
-connect('graphene-mongo-example', host='mongomock://localhost', alias='default')
+# TODO: DELETE WHEN DONE
+import scrape_data
+
+connect("graphene-mongo-example", host="mongomock://localhost", alias="default")
 
 
 def init_db():
@@ -11,12 +14,30 @@ def init_db():
     Creates test data in an local database for development purposes.
     """
     # Colleges
-    engineering = College(name="Engineering", description="A national leader in engineering education, the College of Engineering promotes project-based learning to link theory with hands-on practice to solve the future’s most pressing technical challenges.")
-    business = College(name="College of Business", description="At the Orfalea College of Business, you gain expertise in the fields of management, finance, economics and entrepreneurship by working with real companies in your community and developing your own business ideas.")
-    agriculture = College(name="Agriculture, Food, and Environmental Sciences", description="One of the nation’s most prestigious undergraduate agriculture programs, the College of Agriculture, Food and Environmental Sciences takes you into the field, the farm and the forest to apply cutting-edge techniques to the ancient challenge of feeding a growing society.")
-    architecture = College(name="Architecture and Environmental Design", description="The College of Architecture and Environmental Design offers a hands-on approach to working in all aspects of the built environment, from designing homes and gardens to planning cities.")
-    liberal_arts = College(name="Liberal Arts", description="The College of Liberal Arts houses fifteen departments offering programs in the humanities, social sciences, communications and performing arts that share focus on the evolving nature of human experience and expression in the modern world.")
-    science_math = College(name="Science and Mathematics", description="The College of Science and Mathematics prepares you to become a leader in an increasingly scientific and technological society through education, research and practical experience.")
+    engineering = College(
+        name="Engineering",
+        description="A national leader in engineering education, the College of Engineering promotes project-based learning to link theory with hands-on practice to solve the future’s most pressing technical challenges.",
+    )
+    business = College(
+        name="College of Business",
+        description="At the Orfalea College of Business, you gain expertise in the fields of management, finance, economics and entrepreneurship by working with real companies in your community and developing your own business ideas.",
+    )
+    agriculture = College(
+        name="Agriculture, Food, and Environmental Sciences",
+        description="One of the nation’s most prestigious undergraduate agriculture programs, the College of Agriculture, Food and Environmental Sciences takes you into the field, the farm and the forest to apply cutting-edge techniques to the ancient challenge of feeding a growing society.",
+    )
+    architecture = College(
+        name="Architecture and Environmental Design",
+        description="The College of Architecture and Environmental Design offers a hands-on approach to working in all aspects of the built environment, from designing homes and gardens to planning cities.",
+    )
+    liberal_arts = College(
+        name="Liberal Arts",
+        description="The College of Liberal Arts houses fifteen departments offering programs in the humanities, social sciences, communications and performing arts that share focus on the evolving nature of human experience and expression in the modern world.",
+    )
+    science_math = College(
+        name="Science and Mathematics",
+        description="The College of Science and Mathematics prepares you to become a leader in an increasingly scientific and technological society through education, research and practical experience.",
+    )
 
     engineering.save()
     business.save()
@@ -25,28 +46,32 @@ def init_db():
     liberal_arts.save()
     science_math.save()
 
-    
-
     # Departments (only engineering for now)
-    eng_departments = [Department(name=name, college=engineering) for name in [
-    "Aerospace Engineering",
-    "Architectural Engineering",
-    "Biomedical Engineering",
-    "BioResource and Agricultural Engineering",
-    "Civil Engineering",
-    "Computer Engineering",
-    "Electrical Engineering",
-    "Environmental Engineering",
-    "General Engineering",
-    "Industrial Engineering",
-    "Liberal Arts and Engineering Studies",
-    "Manufacturing Engineering",
-    "Materials Engineering",
-    "Mechanical Engineering",
-    "Software Engineering"
-    ]]
+    eng_departments = [
+        Department(name=name, college=engineering)
+        for name in [
+            "Aerospace Engineering",
+            "Architectural Engineering",
+            "Biomedical Engineering",
+            "BioResource and Agricultural Engineering",
+            "Civil Engineering",
+            "Computer Engineering",
+            "Electrical Engineering",
+            "Environmental Engineering",
+            "General Engineering",
+            "Industrial Engineering",
+            "Liberal Arts and Engineering Studies",
+            "Manufacturing Engineering",
+            "Materials Engineering",
+            "Mechanical Engineering",
+            "Software Engineering",
+        ]
+    ]
 
-    cs_dep = Department(name="Computer Science & Software Engineering", description=" degree in computer science prepares you to design and develop computer technologies such as operating systems, websites and mobile apps, artificial intelligence suites, software for robotics, search engines and more.Modern labs help you gain a practical understanding of computer science – from learning algorithmic problem solving to the high-level programming languages. Individual and team projects, as well as internships and co-ops throughout California's technology hubs, reinforce the concepts learned and provide you the opportunity to apply and communicate your knowledge. The program is accredited by the Engineering Accreditation Commission of ABET.")
+    cs_dep = Department(
+        name="Computer Science & Software Engineering",
+        description=" degree in computer science prepares you to design and develop computer technologies such as operating systems, websites and mobile apps, artificial intelligence suites, software for robotics, search engines and more.Modern labs help you gain a practical understanding of computer science – from learning algorithmic problem solving to the high-level programming languages. Individual and team projects, as well as internships and co-ops throughout California's technology hubs, reinforce the concepts learned and provide you the opportunity to apply and communicate your knowledge. The program is accredited by the Engineering Accreditation Commission of ABET.",
+    )
     eng_departments.append(cs_dep)
 
     for dep in eng_departments:
@@ -94,15 +119,23 @@ The CDSM provides an opportunity for both statistics and computer science studen
 
     """
     cs_progs = []
-    cs = Program(name="B.S. Computer Science", description=cs_desc, )
+    cs = Program(name="B.S. Computer Science", description=cs_desc)
     cs_progs.append(cs)
     cs_progs.append(Program(name="M.S. Computer Science", graduate_level=True))
     cs_progs.append(Program(name="B.S. Software Engineering"))
     cs_progs.append(Program(name="B.S. Software Engineering"))
     cs_progs.append(Program(name="Computer Science Minor", type="minor"))
     cs_progs.append(Program(name="Computing for Interactive Arts Minor", type="minor"))
-    cs_progs.append(Program(name="Cross Disciplinary Studies Minor in Bioinformatics", type="minor"))
-    cs_progs.append(Program(name="Cross Disciplinary Studies Minor in Data Science", description=data_sci_desc, type="minor"))
+    cs_progs.append(
+        Program(name="Cross Disciplinary Studies Minor in Bioinformatics", type="minor")
+    )
+    cs_progs.append(
+        Program(
+            name="Cross Disciplinary Studies Minor in Data Science",
+            description=data_sci_desc,
+            type="minor",
+        )
+    )
 
     for prog in cs_progs:
         prog.department = cs_dep
@@ -110,19 +143,45 @@ The CDSM provides an opportunity for both statistics and computer science studen
     cs_dep.programs = cs_progs
     cs_dep.save()
 
-
     # Courses
     csc_courses = []
-    csc_courses.append(Course(name="Fundamentals of Computer Science", course_id="CSC 101", description="Basic principles of algorithmic problem solving and programming using methods of top-down design, stepwise refinement and procedural abstraction. Basic control structures, data types, and input/output. Introduction to the software development process: design, implementation, testing and documentation. The syntax and semantics of a modern programming language.", units=4, usually_offered=["Fall", "Winter", "Spring"]))
-    csc_courses.append(Course(name="Data Structures", description="Introduction to data structures and analysis of algorithms. Abstract datatypes. Specification and implementation of advanced data structures. Theoretical and empirical analysis of recursive and iterative algorithms. Software performance evaluation and testing techniques.", course_id="CSC 202", units=4, usually_offered=["Winter"]))
-    csc_courses.append(Course(name="Project-Based Object-Oriented Programming and Design", description="Object-oriented programming and design with applications to project construction. Introduction to class design, interfaces, inheritance, generics, exceptions, streams, and testing.", course_id="CSC 203", units=4, usually_offered=["Winter"]))
+    csc_courses.append(
+        Course(
+            name="Fundamentals of Computer Science",
+            course_id="CSC 101",
+            description="Basic principles of algorithmic problem solving and programming using methods of top-down design, stepwise refinement and procedural abstraction. Basic control structures, data types, and input/output. Introduction to the software development process: design, implementation, testing and documentation. The syntax and semantics of a modern programming language.",
+            units=4,
+            usually_offered=["Fall", "Winter", "Spring"],
+        )
+    )
+    csc_courses.append(
+        Course(
+            name="Data Structures",
+            description="Introduction to data structures and analysis of algorithms. Abstract datatypes. Specification and implementation of advanced data structures. Theoretical and empirical analysis of recursive and iterative algorithms. Software performance evaluation and testing techniques.",
+            course_id="CSC 202",
+            units=4,
+            usually_offered=["Winter"],
+        )
+    )
+    csc_courses.append(
+        Course(
+            name="Project-Based Object-Oriented Programming and Design",
+            description="Object-oriented programming and design with applications to project construction. Introduction to class design, interfaces, inheritance, generics, exceptions, streams, and testing.",
+            course_id="CSC 203",
+            units=4,
+            usually_offered=["Winter"],
+        )
+    )
 
     for course in csc_courses:
         course.department = cs_dep
     for i in range(1, len(csc_courses)):
-        csc_courses[i].prerequisites = [csc_courses[i-1]]
+        csc_courses[i].prerequisites = [csc_courses[i - 1]]
     for course in csc_courses:
         course.save()
 
     cs.curriculum = csc_courses
     cs.save()
+
+    # TODO: delete when done
+    scrape_data.main()
