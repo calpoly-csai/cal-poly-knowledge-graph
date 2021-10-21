81005bde66d1bd54929e45563665f298aee60298
Waidhoferj <jwaidhof@calpoly.edu>
2021-10-20 23:15:18 -0700

    added visualizations

--- app.py
+++ app.py
@@ -1,19 +1,24 @@
 # flask_graphene_mongo/app.py
 from tests.local_database import init_db
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_graphql import GraphQLView
 from schema import schema
 from argparse import ArgumentParser
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="")
 app.debug = True
 
 app.add_url_rule(
-    '/graphql',
-    view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True)
+    "/graphql", view_func=GraphQLView.as_view("graphql", schema=schema, graphiql=True)
 )
 
-if __name__ == '__main__':
+
+@app.route("/visualize")
+def display_api():
+    return send_from_directory("pages", "voyager.html")
+
+
+if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("--prod", type=bool, default=False)
     args = parser.parse_args()
